d8286cbbd7fde84ac20e4e344542b7e264f723e9
Flávia Souza <88092488+flaviasouzza@users.noreply.github.com>
2022-04-05 19:14:39 -0300

    Add files via upload

---
+++ DiferençaDeIdadeLogicadeProgramação.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class DiferençaDeIdadeLogicadeProgramação {
+    public static void main (String [] args) {
+        int age1 = 28;
+        int age2 = 25;
+        int difference = age1 - age2;
+
+        System.out.println("The difference of age is " + difference);
+    }
+
+}
